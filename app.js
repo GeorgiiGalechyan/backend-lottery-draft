4a5193b5cb8ab26826300fec176feaa265d25a7c
Georgii Galechyan <galechyan1991@gmail.com>
2022-02-20 23:34:14 +0400

    Работаем с маршрутизацией и модулем авторизации. Сощдали папку routes в которой лежит auth.js - модуль отвечающий за авторизаци. В файл app.js, отвечающий за наш сервер, мы импортировали модуль авторизации auth.js в константу authRoutes, после чего зарегестрировали authRoutes и дали ему виртуальный адрес (путь) через функцию app.use.

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-'use strict'
+'use strict' // строгий режим
 
-const express = require('express')
+const express = require('express') // кодключаем библиотеку express в константу express
+const authRoutes = require('./routes/auth') // регистрируем роуты (маршруты)
 const app = express()
+
+// app.use позволяет подключать плагины или роуты
+
+app.use('/api/auth', authRoutes)
 
 module.exports = app
---
+++ routes/auth.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const express = require('express')
+const router = express.Router()
+
+// localhost:5000/api/auth/login
+router.get('/login', (req, res) => {
+  res.status(200).json({
+    login: true,
+  })
+})
+
+module.exports = router
