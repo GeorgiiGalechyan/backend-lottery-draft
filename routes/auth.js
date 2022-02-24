766e424cb16e42360a0e96e82fa64c9a3f86f6ab
Georgii Galechyan <galechyan1991@gmail.com>
2022-02-25 00:35:43 +0400

    Пару суток не кодил, забыл на чем остановился. Сейчас решил закоммитить последние изменения, какие-би они не были, а дальше разберемся

--- routes/auth.js
+++ routes/auth.js
@@ -4,11 +4,13 @@
 
 // см. картинку по адресу https://mdn.mozillademos.org/files/14456/MVC%20Express.png
 
-const express = require('express') // подключаем библиотеку express в константу express
-const controller = require('../controller/auth') // импортирууем модуль
+const express = require('express') // подключаем библиотеку express в константу express, для тго чтобы потом использовать express.Router()
+const controller = require('../controller/auth') // импортируем модуль контроллера, который исполняется при переходе на url маршрута
 const router = express.Router() // express.Router - класс для создания модульных, монтируемых обработчиков маршрутов
 
 // localhost:5000/api/auth/login
-router.get('/login', controller.login) // функция сработает когда перейдём по адресу localhost:5000/api/auth/login
+router.post('/login', controller.login) // функция-обрабочик маршрут. Сработает когда перейдём по адресу localhost:5000/api/auth/login
+
+router.post('/register', controller.register) // функция-обрабочик маршрут. Сработает когда перейдём по адресу localhost:5000/api/auth/register
 
 module.exports = router
