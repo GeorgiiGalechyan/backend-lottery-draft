fc84d9ecd4d9f50b4daa37b3028a91b49f39b38f
Georgii Galechyan <galechyan1991@gmail.com>
2022-02-26 02:41:56 +0400

    добавил новые файлы контроллеров и rotes, обновил app.js

---
+++ controller/statistics.js
@@ -0,0 +1,9 @@
+'use strict'
+
+// Контроллер - объект, отвечающий за формирование результата по запрошенному маршруту.
+
+module.exports.getGlobalStatistics = function (req, res) {}
+
+module.exports.getMeStatistics = function (req, res) {}
+
+module.exports.deleteMeStatistics = function (req, res) {}
---
+++ routes/statistics.js
@@ -0,0 +1,12 @@
+'use strict'
+
+/*Роут (маршрут) - содержит информацию о будущей странице, её путь, какой контроллер использовать, заголовок, требования и т.д. */
+
+const express = require('express')
+const controller = require('../controller/statistics')
+const router = express.Router()
+
+// router.post('/login', controller.login) // функция-обрабочик маршрут. Сработает когда перейдём по адресу localhost:5000/api/auth/login
+// router.post('/register', controller.register)
+
+module.exports = router
