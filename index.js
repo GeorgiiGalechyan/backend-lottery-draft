c8a32b964a4f55545952398709526153231b00fd
Georgii Galechyan <galechyan1991@gmail.com>
2022-02-25 15:51:36 +0400

    добавлена библиотека dotenv. библиотека позволяет создавать переменные среды, которые указываются в файле с разрешением .env. Файл .env создаётся в корне проекта. добавил файл в корень проекта, внёс соответствующие изменения в файл index.js.

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 'use strict'
 
+const { config } = require('dotenv')
+const { path } = require('./app')
 // В index.js импортируется модуль app.js, который содержит библиотеку express.js и маршруты (routes).
 // При запуске index.js запускается сервер c прослушкой через функцию app.listen([port[, host[, backlog]]][, callback]
 
 const app = require('./app')
+require('dotenv').config() // модуль dotenv - позволяет создавать переменные среды в файле в формате .env
+// {path: path/filename'}
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 6000
+const host = process.env.HOST || localhostDefault
 
-app.listen(port, () => console.log(`Server is starting on port ${port}`))
+app.listen(port, () => console.log(`Server is listening ${host}: ${port}`))
